1ba37f8ce60d4edf94673579baf92a611719e693
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-05-10 00:49:16 +0200

    configured tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import styles from "@styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className={styles.title}>Your Next.JS boilerplate</h1>
+      <h1 className={styles.title}>
+        Your <span className="text-6xl font-bold ">Next.JS</span> boilerplate
+      </h1>
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./pages/*.{ts,tsx,js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/components/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

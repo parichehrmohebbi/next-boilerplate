b38aa2d73dd07420844c4fa9a5e918e1deca7c12
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-05-09 15:47:11 +0200

    jest confihs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+    video: false,
   },
   component: {
     devServer: {
-      framework: 'next',
-      bundler: 'webpack',
+      framework: "next",
+      bundler: "webpack",
     },
   },
-})
+});

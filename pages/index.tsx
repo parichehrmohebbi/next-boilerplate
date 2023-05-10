25e2c378482db04ce1bb112fafa8bc0119ec7cbb
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-05-10 08:28:21 +0200

    remove img

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,6 @@ export default function Home() {
       <h1 className={styles.title}>
         Your <span className="text-6xl font-bold ">Next.JS</span> boilerplate
       </h1>
-      <Image
-        src="/assets/images/vercel.svg"
-        alt="Vercel Logo"
-        className={styles.vercelLogo}
-        width={100}
-        height={24}
-        priority
-      />
     </>
   );
 }
